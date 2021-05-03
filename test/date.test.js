2602df3fb058813d1b6219fef7390ef2d2266bff
Brahim <brahimcasas@hotmail.es>
2021-05-03 16:27:57 +0200

    testing

--- test/date.test.js
+++ test/date.test.js
@@ -9,8 +9,10 @@ test("gets current day", () => {
 
 test("gets current month", () => {
   // TODO implement
+  expect(date.getCurrentMonth()).toBe(new Date().getMonth());
 });
 
 test("gets current year", () => {
   // TODO implement
+  expect(date.getCurrentYear()).toBe(new Date().getFullYear());
 });
