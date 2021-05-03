c63014f6c63dcd30a66fce42a8a2809666ca95ce
Miguel Perez Martinez <migueldw2.0@gmail.com>
2021-05-03 17:25:01 +0200

    finished

--- test/date.test.js
+++ test/date.test.js
@@ -1,16 +1,19 @@
 const date = require("../src/date");
+var moment = require("moment");
 
 // Date tests
 
 test("gets current day", () => {
   // TODO implement
-  // expect(date.getCurrentDay()).toBe(new Date().getDay());
+  expect(date.getCurrentDay()).toBe(moment().format("dddd"));
 });
 
 test("gets current month", () => {
   // TODO implement
+  expect(date.getCurrentMonth()).toBe(moment().format("MMMM"));
 });
 
 test("gets current year", () => {
   // TODO implement
+  expect(date.getCurrentYear()).toBe(moment().format("YYYY"));
 });
--- test/util.test.js
+++ test/util.test.js
@@ -1,3 +1,4 @@
+const { createArray, toCamelCase, getIndexOf } = require("../src/util");
 const util = require("../src/util");
 
 // Util tests
@@ -7,15 +8,17 @@ test("creates an array of 6 elements lol", () => {
   // expect(util.createArray(6).length).toBe(6);
 
   // just to let it go red :) cool! now your turn remove this line an implement it you have the clue over this.
-  expect(1).toBe(2);
+  expect(util.createArray(5).length).toBe(6);
 });
 
 test("gets the index of a given value", () => {
   var arrayColors = ["red", "blue", "green"];
   // TODO implement
+  expect(util.getIndexOf(arrayColors, "blue")).toBe(1);
 });
 
 test("moves a string to camel case", () => {
   var string = "User name";
   // TODO implement
+  expect(util.toCamelCase("User name")).toBe("userName");
 });
