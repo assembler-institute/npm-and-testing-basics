cb7984221549bfcc326e0938591f75aaf8f1ab8f
Enrique Hidalgo <koko.freelance@gmail.com>
2021-09-01 14:33:11 +0100

    testing working, updated readme

--- test/date.test.js
+++ test/date.test.js
@@ -4,13 +4,15 @@ const date = require("../src/date");
 
 test("gets current day", () => {
   // TODO implement
-  // expect(date.getCurrentDay()).toBe(new Date().getDay());
+  expect(date.getCurrentDay("2021-09-01")).toBe("Wednesday");
 });
 
 test("gets current month", () => {
   // TODO implement
+  expect(date.getCurrentMonth("2021-09-01")).toBe("September");
 });
 
 test("gets current year", () => {
   // TODO implement
+  expect(date.getCurrentYear("2021-09-01")).toBe("2021");
 });
