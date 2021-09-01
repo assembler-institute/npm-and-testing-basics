a89e3bf5c09e7088379f1400cc22671752260a20
andarbech <andarbech1.com>
2021-09-01 17:15:13 +0200

    finish proyect

--- src/date.js
+++ src/date.js
@@ -1,24 +1,25 @@
-var moment = require("moment");
+const moment = require("moment");
 
 /**
  * TODO: all getters, all get method must return a value
  * */
 
 function getWeekDay() {
-  // TODO implement
-  // return moment().day();
+    return moment().day();
 }
 
 function getCurrentMonth() {
-  // TODO implement
+
+    return moment().month()
 }
 
 function getCurrentYear() {
-  // TODO implement
+
+    return moment().year()
 }
 
 module.exports = {
-  getCurrentDay: getWeekDay,
-  getCurrentMonth: getCurrentMonth,
-  getCurrentYear: getCurrentYear,
+    getCurrentDay: getWeekDay,
+    getCurrentMonth: getCurrentMonth,
+    getCurrentYear: getCurrentYear,
 };
