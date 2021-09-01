d49260f7ca398976fca1350d060024d1e6b8bbdb
Haroon Ahmad <haroon334@gmail.com>
2021-09-01 15:26:57 +0200

    Finished

--- src/util.js
+++ src/util.js
@@ -5,20 +5,20 @@ const lodash = require("lodash");
  * */
 
 function createArray(arraySize) {
-  // TODO implement
-  // return lodash.times(arraySize, lodash.uniqueId.bind(null, 'ball_'));
+    // TODO implement
+    return lodash.times(arraySize, lodash.uniqueId.bind(null, "ball_"));
 }
 
 function toCamelCase(string) {
-  // TODO implement
+    return lodash.camelCase(string);
 }
 
 function getIndexOf(array, value) {
-  // TODO implement
+    return lodash.indexOf(array, value);
 }
 
 module.exports = {
-  createArray: createArray,
-  toCamelCase: toCamelCase,
-  getIndexOf: getIndexOf,
+    createArray: createArray,
+    toCamelCase: toCamelCase,
+    getIndexOf: getIndexOf,
 };
