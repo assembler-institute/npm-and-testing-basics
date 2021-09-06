0ded5022c6145d85d7ff49760994aa1b9bd6c903
Paola3stefania <paolaestefania.decampos@gmail.com>
2021-09-06 19:42:38 +0200

    jest testing

--- test/util.test.js
+++ test/util.test.js
@@ -2,12 +2,11 @@ const util = require("../src/util");
 
 // Util tests
 
-test("creates an array of 6 elements lol", () => {
-  // TODO implement
-  // expect(util.createArray(6).length).toBe(6);
+test("creates an array of 6 elements", () => {
 
-  // just to let it go red :) cool! now your turn remove this line an implement it you have the clue over this.
-  expect(1).toBe(2);
+  expect(util.createArray(6).length).toBe(6);
+
+
 });
 
 test("gets the index of a given value", () => {
