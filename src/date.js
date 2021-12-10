208502c3def451b44012bfc4a1e0cfa75317d5ed
sergiogt93 <sergio.ber0556@gmail.com>
2021-12-10 14:10:50 +0100

    Finished test functions

--- src/date.js
+++ src/date.js
@@ -4,17 +4,22 @@ var moment = require("moment");
  * TODO: all getters, all get method must return a value
  * */
 
+//Return the day of the week
 function getWeekDay() {
   // TODO implement
-  // return moment().day();
+  return moment().day();
 }
 
+//Return the current month
 function getCurrentMonth() {
   // TODO implement
+  return moment().month();
 }
 
+//Return the current year
 function getCurrentYear() {
   // TODO implement
+  return moment().year();
 }
 
 module.exports = {
--- src/util.js
+++ src/util.js
@@ -4,17 +4,22 @@ const lodash = require("lodash");
  * TODO: all getters, all get method must return a value
  * */
 
+//Create to new array
 function createArray(arraySize) {
   // TODO implement
-  // return lodash.times(arraySize, lodash.uniqueId.bind(null, 'ball_'));
+  return lodash.times(arraySize, lodash.uniqueId.bind(null, 'ball_'));
 }
 
+//Convert the string a camelCase
 function toCamelCase(string) {
   // TODO implement
+  return lodash.camelCase(string);
 }
 
+//Return the value of index array indicated
 function getIndexOf(array, value) {
   // TODO implement
+  return lodash.indexOf(array, value);
 }
 
 module.exports = {
